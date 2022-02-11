a0f9bf0e54ed3d1deb99225770c9b00103d19f1a
1ombfinance <99106573+1ombfinance@users.noreply.github.com>
2022-02-10 20:17:20 -0800

    token list

--- src/config/token-lists.ts
+++ src/config/token-lists.ts
@@ -17,7 +17,7 @@ const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
 const KLEROS_LIST = 't2crtokens.eth'
 export const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json'
-const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
+const SET_LIST = 'https://raw.githubusercontent.com/1ombfinance/tokenlist/main/default-token-list.json'
 const UMA_LIST = 'https://umaproject.org/uma.tokenlist.json'
 const WRAPPED_LIST = 'wrapped.tokensoft.eth'
 const DHEDGE_LIST = 'https://list.dhedge.eth.link'
@@ -25,25 +25,9 @@ const ARBITRUM_LIST = 'https://bridge.arbitrum.io/token-list-42161.json'
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  COMPOUND_LIST,
-  AAVE_LIST,
-  CMC_ALL_LIST,
-  CMC_STABLECOIN,
-  UMA_LIST,
-  YEARN_LIST,
-  SYNTHETIX_LIST,
-  WRAPPED_LIST,
   SET_LIST,
-  COINGECKO_LIST,
-  KLEROS_LIST,
-  NFTX_LIST,
-  OPTIMISM_LIST,
-  GEMINI_LIST,
-  ARBITRUM_LIST,
-  OPTIMISM_LIST,
-  DHEDGE_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [NFTX_LIST, YEARN_LIST, GEMINI_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = [SET_LIST]
